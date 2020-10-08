0dbeb504be1546e94eca399ff3f5a6f683ed3388
Israel Pires Rosa <piresr.israel@gmail.com>
2020-10-08 10:22:33 -0300

    responsive display

--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -467,8 +467,12 @@ export const ServicesContent = styled.div`
 
 export const Portfolio = styled.div`
   display: flex;
+  flex-wrap: wrap;
   width: 100vw;
   height: 100vh;
+  align-items: center;
+  justify-content: center;
+  background: white;
 `;
 
 export const PortfolioImg = styled.div`
@@ -477,14 +481,20 @@ export const PortfolioImg = styled.div`
   padding: var(--margin-width);
   justify-content: center;
   align-items: center;
-  min-width: 50%;
+  min-width: 50vh;
+  max-height: 50vh;
+  background: white;
+  object-fit: cover;
 
   >div {
     display: flex;
+    flex: 1;
     justify-content: center;
     align-items: center;
-    min-width: 100%;
-    max-height: 500px;
+    min-width: 30%;
+    max-width: 50vh ;
+    max-height: 100%;
+    object-fit: cover;
   }
 `;
 
@@ -493,17 +503,19 @@ export const PortfolioOptions = styled.ul`
   list-style: none;
   flex-direction: column;
   flex: 1;
-  justify-content: center;
+  max-height: 100vh;
   overflow-y: scroll;
+  padding: 20px 70px 20px var(--margin-width);
+  overflow-y: auto;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
   align-items: center;
-  margin: 30px 0px;
-  padding: 0px 50px 0px 0px;
-  min-width: 50%;
 
   li {
     display: flex;
     flex: 1;
     cursor: pointer;
+    scroll-snap-align: start;
 
     flex-direction: column;
     margin: 5px 0px;
@@ -524,5 +536,14 @@ export const PortfolioOptions = styled.ul`
         margin-bottom: 15px;
       }
     }
+    :last-child {
+      margin-bottom: 40px;
+    }
+  }
+
+  @media(max-width: 800px) {
+    max-height: 50vh;
+    min-width: fit-content;
+    padding: 20px var(--margin-width);
   }
 `;
